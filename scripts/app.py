bef1f52cb1fd9409b17389e18402c5805aa1d69a
Jaiden <jaidendutoit@gmail.com>
2025-03-28 15:57:08 +0200

    restarted index.html

--- scripts/app.py
+++ scripts/app.py
@@ -79,7 +79,7 @@ def transactions():
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
